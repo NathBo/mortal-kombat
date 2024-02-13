810b1e4495507f80ba152832af25204e79231734
NathBo <nboyer200492@gmail.com>
2024-02-13 22:07:47 +0100

    better fan sound

--- home.js
+++ home.js
@@ -91,7 +91,6 @@ function main(){
 			ctx.save();
 			var x = (this.x+decalagex-camerax-this.orientation*this.width/2+shakex)*this.orientation;
 			var y = ground-this.y+shakey-45;
-			console.log(this.rotation);
 			ctx.scale(2*this.orientation,2);
 			ctx.translate(x+10,y+22);
 			ctx.rotate(Math.PI*this.rotation/180);
@@ -99,7 +98,7 @@ function main(){
 			ctx.setTransform(1, 0, 0, 1, 0, 0);
 			ctx.scale(1,1);
 			ctx.restore();
-			this.dur--;
+			if(gamefreeze==0){this.dur--;}
 			if(this.dur==0){this.delete();return;}
 		}
 
@@ -920,12 +919,13 @@ function main(){
 	kitana_coups.set("jskick",{slag : 8, fdur : 15, elag : 4, degats : 13, hitstun : 32, hurtx : 0.8, hurty : 0, hitboxxs : 10, hitboxxe : 33,hitboxys : -20, hitboxye : 30, landinglag : 8, blockstun : 10, blockx : 0.4, hiteffect : "none", hitboxxeyscaling : 0, hitlag : 8, hitsound : "hhit", blood : "lblood", damageonblock : 1});
 	kitana_coups.set("jpunch",{slag : 5, fdur : 10, elag : 6, degats : 9, hitstun : 20, hurtx : 1.5, hurty : 0, hitboxxs : -5, hitboxxe : 58,hitboxys : -40, hitboxye : 5, landinglag : 8, blockstun : 10, blockx : 0.4, hiteffect : "none", hitboxxeyscaling : 0, hitlag : 7, hitsound : "lhit", blood : "lblood", damageonblock : 1});
 	kitana_coups.set("grab",{slag : 5, fdur : 3, elag : 12, degats : 15, hitstun : 22, hurtx : 0.9, hurty : 0, hitboxxs : 5, hitboxxe : 28,hitboxys : 0, hitboxye : 50, blockstun : 12, blockx : 0.6, hiteffect : "grab", hitboxxeyscaling : 0, hitlag : 5, hitsound : "lhit", blood : "lblood", damageonblock : 1});
-	kitana_coups.set("fanthrow",{slag : 25, fdur : 0, elag : 14, degats : 8, hitstun : 22, hurtx : 0.9, hurty : 0, hitboxxs : -15, hitboxxe : 15,hitboxys : -20, hitboxye : 15, blockstun : 12, blockx : 0.6, hiteffect : "projectile", hitboxxeyscaling : 0, hitlag : 5, hitsound : "lhit", blood : "lblood", damageonblock : 1});
+	kitana_coups.set("fanthrow",{slag : 25, fdur : 0, elag : 14, degats : 8, hitstun : 22, hurtx : 0.9, hurty : 0, hitboxxs : -15, hitboxxe : 15,hitboxys : -20, hitboxye : 15, blockstun : 12, blockx : 0.6, hiteffect : "projectile", hitboxxeyscaling : 0, hitlag : 3, hitsound : "fan", blood : "lblood", damageonblock : 2});
 
 	var sounds_eff = new Map();
 	sounds_eff.set("lhit",[document.querySelector('#lhitwav1'),document.querySelector('#lhitwav2'),document.querySelector('#lhitwav3')]);
 	sounds_eff.set("mhit",[document.querySelector('#mhitwav1'),document.querySelector('#mhitwav2'),document.querySelector('#mhitwav3')]);
 	sounds_eff.set("hhit",[document.querySelector('#hhitwav1'),document.querySelector('#hhitwav2'),document.querySelector('#hhitwav3')]);
+	sounds_eff.set("fan",[document.querySelector('#fanwav')]);
 
 	function play_sound_eff(s){
 		let l = sounds_eff.get(s);
