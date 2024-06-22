9c362a938d9807bebf4988b59d90791a74230c3d
NathBo <nboyer200492@gmail.com>
2024-06-22 22:13:48 +0200

    coups normaux

--- home.js
+++ home.js
@@ -866,7 +866,7 @@ function main(){
 				else if(this.grabbing == Math.floor(this.charac.grabfdur*5/7)){
 					other.grabbed=0;
 					other.falling=10;
-					if(this.perso=="scorpion"){
+					if(this.perso=="scorpion" || this.perso=="subzero"){
 						var stats = this.charac.coups.get("grab");
 						other.y = 30;
 						other.hurted = stats.hitstun;
@@ -1622,7 +1622,7 @@ function main(){
 
 			if(other.grabbed && this.grabbed==0){
 				var angletot = Math.PI;
-				if(this.perso=="scorpion"){
+				if(this.perso=="scorpion" || this.perso=="subzero"){
 					angletot = Math.PI*1/2;
 				}
 				if(this.grabbing<=this.charac.grabfdur*1/7){var othercost = "grabbed1";}
@@ -2226,7 +2226,7 @@ function main(){
 
 	characteristics.set("subzero",{png : subskins,coordinates : subcoordinates, sex : "m", standnframes : 10, rollspeed : 5, hkickstartnframe : 3, hkickendnframe : 2, kicknframe : 4, grabxdist : 32, grabydist : 38, stunnframes : 5, walknframes : 9, icon : scorpioniconpng, namewav : document.querySelector('#scorpionwav'),
 		width : 39, height : 103,vitesse : 3,jumpxspeed : 3.4,backmovnerf : 0.9, gravity : 0.41, jumpforce : 9.1,jumpsquat : 3, shorthop : 6.3, friction:0.17, hurtcontrol : 0.18,
-		airdrift : 0.13, airmaxspeed : 1.8, airdodgespeed : 5.7, airdodgefdur : 15, landinglag : 9, coups : scorpion_coups, pv : 95, getupfdur : 36, grabfdur : 20, grabdeg : 12, vicposframes : 2, vicposfdur : 14, cds : [150,180,180,120], icons : [spearthrowiconpng,thundergodiconpng,hellgatesiconpng,legtakedowniconpng], voiceactor : "male"});
+		airdrift : 0.13, airmaxspeed : 1.8, airdodgespeed : 5.7, airdodgefdur : 15, landinglag : 9, coups : subzero_coups, pv : 95, getupfdur : 36, grabfdur : 20, grabdeg : 12, vicposframes : 2, vicposfdur : 14, cds : [150,180,180,120], icons : [spearthrowiconpng,thundergodiconpng,hellgatesiconpng,legtakedowniconpng], voiceactor : "male"});
 	
 
 
@@ -2249,7 +2249,7 @@ function main(){
 	var roundwonsj1 = 0; var roundwonsj2 = 0;
 	var finishhim = 0; var fatalitywasdone = false; var fatalitysreen = 0;
 	var persoschoisis = ["kitana","raiden"]; var persolocked = [0,0]; var persosovered = [0,0];
-	var musiqueon = false; var soundeffon = false; var introon = true; var timer = 0; var timer_init = 99*60;
+	var musiqueon = true; var soundeffon = true; var introon = true; var timer = 0; var timer_init = 99*60;
 	var liste_persos = ["raiden","kitana","scorpion","subzero"];
 	var chartimer = 0; var chartimercycle = 3; var difficultynames = ["Easy","Normal","Hard","Insane","Terminator"];
 	var is_in_charc_screen = true; var lockincountdown = 0; var lockincountdownfdur = 40; var controlafaire = -1; var key = "";
